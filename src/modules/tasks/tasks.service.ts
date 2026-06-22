3e0bb4dfe2d8cd478857cfca6fdb5ac7c2c4c9b4
Paulo Henrique <phttm0501@gmail.com>
2026-06-22 04:06:23 -0400

    Tasks(prisma, DTO, service, controller, module)

---
+++ src/modules/tasks/tasks.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { TaskDTO } from './tasks.dto'
+
+@Injectable()
+export class TasksService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAllByProject(projectId: string) {
+    return this.prisma.task.findMany({
+      where: {
+        projectId,
+      },
+    })
+  }
+
+  async findById(projectId: string, taskId: string) {
+    return this.prisma.task.findFirst({
+      where: {
+        id: taskId,
+        projectId,
+      },
+    })
+  }
+
+  async create(projectId: string, data: TaskDTO) {
+    return this.prisma.task.create({
+      data: {
+        ...data,
+        projectId,
+      },
+    })
+  }
+
+  async update(projectId: string, taskId: string, data: any) {
+    return this.prisma.task.updateMany({
+      where: {
+        id: taskId,
+        projectId,
+      },
+      data,
+    })
+  }
+
+  async delete(projectId: string, taskId: string) {
+    await this.prisma.task.deleteMany({
+      where: {
+        id: taskId,
+        projectId,
+      },
+    })
+  }
+}
