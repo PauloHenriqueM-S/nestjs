552546ccd65f163e1a910c8fbbc6184d7bed2780
Paulo Henrique <phttm0501@gmail.com>
2026-06-25 01:39:49 -0400

    Users DTO

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -11,6 +11,8 @@ import {
   Post,
   Put,
 } from '@nestjs/common'
+import { ApiResponse } from '@nestjs/swagger'
+import { CreateUserDTO, UpdateUserDTO, UserFullDTO, UserListItemDTO } from './users.dto'
 import { UsersService } from './users.service'
 
 @Controller({
@@ -21,11 +23,13 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiResponse({ type: [UserListItemDTO] })
   findAll() {
     return this.usersService.findAll()
   }
 
   @Get(':userId')
+  @ApiResponse({ type: UserFullDTO })
   async findOne(@Param('userId', ParseUUIDPipe) userId: string) {
     const user = await this.usersService.findById(userId)
     if (!user) {
@@ -36,12 +40,12 @@ export class UsersController {
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  async create(@Body() data: any) {
+  async create(@Body() data: CreateUserDTO) {
     return this.usersService.create(data)
   }
 
   @Put(':userId')
-  async update(@Param('userId', ParseUUIDPipe) userId: string, @Body() data: any) {
+  async update(@Param('userId', ParseUUIDPipe) userId: string, @Body() data: UpdateUserDTO) {
     return this.usersService.update(userId, data)
   }
 
