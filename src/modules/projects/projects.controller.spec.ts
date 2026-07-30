23127be298f23b98cd504c4c7e41c415e4f16c73
Paulo Henrique <phttm0501@gmail.com>
2026-07-30 02:17:40 -0400

    Project and App tests

--- src/modules/projects/projects.controller.spec.ts
+++ src/modules/projects/projects.controller.spec.ts
@@ -1,18 +1,114 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectsController } from './projects.controller';
+import { ProjectsModule } from './projects.module';
+import { ProjectsService } from './projects.service';
+import { PrismaService } from 'src/prisma.service';
+import { RequestContextService } from 'src/common/services/request-context/request-context.service';
+import {mockPaginationQuery, mockedProjects} from './projects.mocks'
+import { paginateOutput } from 'src/utils/pagination.utils';
+import { Project } from '@prisma/client';
 
 describe('ProjectsController', () => {
   let controller: ProjectsController;
+  let service: ProjectsService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [ProjectsController],
-    }).compile();
+      imports: [ProjectsModule] ,
+    })
+    .overrideProvider(ProjectsService)
+    .useValue(service)
+    .overrideProvider(PrismaService)
+    .useValue({ $connect: jest.fn() })
+    .overrideProvider(RequestContextService)
+    .useValue({ getUserId: jest.fn().mockReturnValue('user-1') })
+    .compile();
 
     controller = module.get<ProjectsController>(ProjectsController);
+    service = module.get<ProjectsService>(ProjectsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+  describe('findAll', () => {
+    it('should return a paginated list of projects', async () => {
+      const mockedRespones = paginateOutput<Project>(
+        mockedProjects,
+        mockedProjects.length,
+        mockPaginationQuery
+      )
+
+      jest.spyOn(service, 'findAll').mockResolvedValue(mockedRespones)
+
+      const response = await controller.findAll()
+
+      expect(response).toEqual(mockedRespones)
+      expect(service.findAll).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('findOne', () => {
+    it('should be able to return a single project by Id', async () => {
+      const project = mockedProjects[0]
+      const projectId = project.id
+      const expectedResult = {
+        ...project,
+        tasks: [],
+      }
+
+      jest.spyOn(service, 'findById').mockResolvedValue(expectedResult)
+
+      const response = await controller.findOne(projectId)
+
+      expect(response).toEqual(expectedResult)
+      expect(service.findById).toHaveBeenCalledWith(projectId)
+      expect(service.findById).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('create', () => {
+    it('should be able to create a new project', async () => {
+      const project = mockedProjects[0]
+
+      jest.spyOn(service, 'create').mockResolvedValue(project)
+
+      const response = await controller.create({
+        name: project.name,
+        description: project.description as string
+      })
+
+      expect(response).toEqual(project)
+      expect(service.create).toHaveBeenCalledTimes(1)
+    })
+
+    it('should be able to handle validation errors', async () => {
+      const error = new Error('Name is required')
+
+      jest.spyOn(service, 'create').mockRejectedValue(error)
+
+      await expect(controller.create({ name: '', description: '' })).rejects.toThrow(error)
+    })
+  })
+
+  describe('update', () => {
+    it('should be able to update a project', async () => {
+      const project = { ...mockedProjects[0], tasks: [] }
+
+      jest.spyOn(service, 'update').mockResolvedValue(project)
+
+      const response = await controller.update(project.id, {
+        name: project.name,
+        description: project.description as string
+      })
+
+      expect(response).toEqual(project)
+      expect(service.update).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('remove', () => {
+    it('should be able to remove a project', async () => {
+      jest.spyOn(service, 'remove').mockImplementation()
+      await controller.remove(mockedProjects[0].id)
+      expect(service.remove).toHaveBeenCalledTimes(1)
+    })
+  })
 });
