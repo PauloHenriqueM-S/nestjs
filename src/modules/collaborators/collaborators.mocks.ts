0341c0e305aeff3057f4cab8ba83e5be7cf4fa17
Paulo Henrique <phttm0501@gmail.com>
2026-08-04 20:48:22 -0400

    Collaborators service and controller test

---
+++ src/modules/collaborators/collaborators.mocks.ts
@@ -0,0 +1,19 @@
+import { faker } from "@faker-js/faker";
+import { ProjectCollaborator } from "@prisma/client";
+import { QueryPaginationDTO } from "src/common/dtos/query-pagination.dto";
+
+export const mockPaginationQuery: QueryPaginationDTO = {
+  page: '1',
+  size: '10',
+}
+
+export const mockedCollaborators = faker.helpers.multiple<ProjectCollaborator>(() => ({
+  id: faker.string.uuid(),
+  role: 'EDITOR',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  projectId: 'project-1',
+  userId: 'user-1',
+}),
+  { count: 5 },
+)
